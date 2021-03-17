427ef84ed3a41139f52caa190062cd4fac60569a
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-03-17 17:10:05 +0800

    Add files via upload

---
+++ 7.10.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main() {
+	float wage, tax, ave, b;
+	int a=0;
+	
+	printf("1.单身\n2.户主\n3.已婚，共有\n4.已婚，离异\n");
+	printf("请输入类别：");
+	while (scanf("%d", &a) == 1 && a != '#') {
+		printf("\n请输入工资：");
+		scanf("%f", &wage);
+		
+		switch (a)
+		{
+		case 1:
+			b = 17850;
+			break;
+		case 2:
+			b = 23900;
+			break;
+		case 3:
+			b = 23900;
+			break;
+		case 4:
+			b = 14875;
+			break;
+		default:
+			printf("\n请正确输入!\n");
+			break;
+		}
+		if (wage < b) {
+			tax = wage * 0.15;
+			ave = wage - tax;
+			printf("税金是：%f\t收入是：%f\n", tax, ave);
+
+		}
+		else
+		{
+			tax = b * 0.15 + ((double)wage - b) * 0.28;
+			ave = wage - tax;
+			printf("税金是：%f\t收入是：%f\n", tax, ave);
+
+		}
+
+	}
+	return 0;
+}
