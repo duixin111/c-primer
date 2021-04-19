0efe81e0fbddd9bbe71e8e1df6cd62038dc5143d
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-04-19 09:35:23 +0800

    谁是凶手（一人说假话）

---
+++ murderer.c
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//
+//int main()
+//{
+//	
+//	for (char murderer = 'A'; murderer <= 'D'; murderer++)
+//	{
+//		if ((murderer != 'A') + (murderer == 'C') + (murderer == 'D') + (murderer != 'D') == 3)
+//			printf("murderer is %c", murderer);
+//	}
+//	return 0;
+//}
+
