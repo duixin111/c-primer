303fe08e887300f51c8d9977b73bcfd47b75a018
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-04-30 11:21:27 +0800

    求出n个值中最大最小值的差值

---
+++ NumDiff.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int max = 0, min = 9999999999, value, num;
+	printf("How many values do you want to enter?\n");
+	scanf_s("%d", &num);
+	for (int i = 0; i < num; i++)
+	{
+		printf("please input number:");
+		scanf_s("%d", &value);
+		max = max < value ? value : max;
+		min = min > value ? value : min;
+	}
+	printf("The difference is %d.", max - min);
+	return 0;
+}
